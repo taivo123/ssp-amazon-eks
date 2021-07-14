51e56e15ec076be2e29ee53ad89af521ef9aa76a
Kevin Coleman <kcaws@amazon.com>
2021-07-14 10:54:33 -0700

    Adding values prop to prometheus addon

--- lib/addons/prometheus/index.ts
+++ lib/addons/prometheus/index.ts
@@ -4,13 +4,31 @@ const DEFAULT_CHART_VERSION = '14.4.0'
 const CHART_REPO = 'https://prometheus-community.github.io/helm-charts'
 
 /**
+ * Properties for the PrometheusAddOn
+ */
+type PrometheusAddOnProps = {
+    /**
+     * The version of the Prometheus Helm chart to deploy. 
+     * @default DEFAULT_CHART_VERSION
+     */
+    version: string
+
+
+    values?: { [map: string]: any }
+}
+
+/**
  * PrometheusAddOn installs Prometheus in an EKS cluster.
  */
 export class PrometheusAddOn implements ClusterAddOn {
+
     private version: string;
 
-    constructor(version?: string) {
-        this.version = version ?? DEFAULT_CHART_VERSION;
+    private values?: { [map: string]: any }
+
+    constructor(props?: PrometheusAddOnProps) {
+        this.version = props?.version ?? DEFAULT_CHART_VERSION;
+        this.values = props?.values
     }
 
     deploy(clusterInfo: ClusterInfo): void {
@@ -19,6 +37,7 @@ export class PrometheusAddOn implements ClusterAddOn {
             repository: CHART_REPO,
             version: this.version,
             namespace: "prometheus",
+            values: this.values
         });
     }
 }
