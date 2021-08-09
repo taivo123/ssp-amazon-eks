d5844bf51a9c21ba2ff7d758a68eb42b01fd55be
Kevin Coleman <kcaws@amazon.com>
2021-08-09 08:54:01 -0700

    Working end to end

--- lib/addons/elasticsearch/index.ts
+++ lib/addons/elasticsearch/index.ts
@@ -4,9 +4,11 @@ import * as es from '@aws-cdk/aws-elasticsearch';
 import * as iam from '@aws-cdk/aws-iam';
 import { KubernetesManifest } from '@aws-cdk/aws-eks';
 import { Effect, PolicyStatement } from '@aws-cdk/aws-iam';
+import request from 'sync-request';
 
 import { ClusterAddOn, ClusterInfo } from "../../stacks/cluster-types";
 import { loadYaml, readYamlDocument } from "../../utils/yaml-utils";
+import { Team } from "../../teams";
 
 /**
  * Configuration options for the external DNS add-on.
@@ -47,13 +49,17 @@ export class ElasticsearchAddOn implements ClusterAddOn {
 
     readonly domainName = 'elasticsearch-logging';
 
-    readonly namespace = 'elasticsearch'
+    readonly namespace = 'logging'
 
     readonly adminUsername = 'admin'
 
     readonly adminPasswordSecretKey = 'elastic-search-password'
 
     readonly version = es.ElasticsearchVersion.V7_10
+
+    private fluentBitSARoleArn: string
+
+    private elasticSearchDomainEndpoint: string
 
     constructor(private readonly options?: ElasticsearchAddOnProps) { }
 
@@ -66,46 +72,45 @@ export class ElasticsearchAddOn implements ClusterAddOn {
         /**
          * Deploy Fluent Bit
          */
-        this.deployFluentBit(clusterInfo, domain.domainEndpoint)
+        this.deployFluentBit(clusterInfo, domain)
+    }
+
+    postDeploy(clusterInfo: ClusterInfo, teams: Team[]): void {
+        /**
+         * Maps the FluentBit IAM Role to the Admin User.   
+         * https://www.eksworkshop.com/intermediate/230_logging/config_es/
+         */
+        this.mapIAMRoleToUser()
     }
 
     protected provisionElasticsearchDomain(clusterInfo: ClusterInfo) {
+        /**
+         * Build our domain Arn.
+         */
         const cluster = clusterInfo.cluster;
-        const domainName = this.options?.domainName ?? this.domainName;
-
-        const namespace = 'logging'
-        const namespaceManifest = this.createNamespace(clusterInfo, namespace)
-        const serviceAccountName = 'fluent-bit-service-account'
-        const sa = cluster.addServiceAccount(serviceAccountName, {
-            name: serviceAccountName,
-            namespace: namespace
-        });
-        sa.node.addDependency(namespaceManifest);
-
         const region = cluster.stack.region
         const accountID = cluster.stack.account
-        const domainArn = `arn:aws:es:${region}:${accountID}:domain/${domainName}}`
-        const policy = new PolicyStatement({
-            effect: Effect.ALLOW,
-            actions: ['es:ESHttp*'],
-            resources: [domainArn]
-        })
-        sa.addToPrincipalPolicy(policy);
+        const domainName = this.options?.domainName ?? this.domainName;
+        const domainArn = `arn:aws:es:${region}:${accountID}:domain/${domainName}`
 
         /**
-         * Setup Elastic Search
+         * Configure IAM policy for the domain.
          */
         const domainPolicy = new PolicyStatement({
             effect: Effect.ALLOW,
             actions: ['es:ESHttp*'],
             principals: [new iam.AnyPrincipal()],
-            resources: [domainArn]
+            resources: [`${domainArn}/*`]
         })
 
+        /**
+         * Deploy the Elasicsearch domain.
+         */
         const masterUserName = this.options?.adminUsername || this.adminUsername
         const adminPasswordSecretKey = this.options?.adminPasswordSecretKey || this.adminPasswordSecretKey
         const masterUserPassword = cdk.SecretValue.secretsManager(adminPasswordSecretKey)
-        return new es.Domain(clusterInfo.cluster.stack, domainName, {
+        const domain = new es.Domain(clusterInfo.cluster.stack, domainName, {
+            domainName,
             version: this.version,
             capacity: {
                 masterNodes: 5,
@@ -134,20 +139,76 @@ export class ElasticsearchAddOn implements ClusterAddOn {
             enforceHttps: true,
             removalPolicy: cdk.RemovalPolicy.DESTROY
         });
+        this.elasticSearchDomainEndpoint = domain.domainEndpoint
+        return domain
     }
-    protected deployFluentBit(clusterInfo: ClusterInfo, domainEndpoint: string) {
-        const cluster = clusterInfo.cluster;
-        const domainName = this.options?.domainName ?? this.domainName;
 
-        // Apply manifest
+    protected deployFluentBit(clusterInfo: ClusterInfo, domain: es.Domain) {
+        /**
+         * Create the FluentBit namespace.
+         */
+        const cluster = clusterInfo.cluster;
+        const namespace = this.options?.namespace ?? this.namespace
+        const namespaceManifest = this.createNamespace(clusterInfo, namespace)
+
+        /**
+         * Create the FluentBut service account.
+         */
+        const serviceAccountName = 'fluent-bit-service-account'
+        const sa = cluster.addServiceAccount(serviceAccountName, {
+            name: serviceAccountName,
+            namespace: namespace
+        });
+        sa.node.addDependency(namespaceManifest)
+
+
+        /**
+         * Create the IAM Policy for the service account 
+         * Allows the SA to make requests to the Elasticsearch domain. 
+         */
+        const policy = new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['es:ESHttp*'],
+            resources: [domain.domainArn]
+        })
+        sa.addToPrincipalPolicy(policy);
+        this.fluentBitSARoleArn = sa.role.roleArn
+
+        /**
+         * Apply the FluentBit manifest.
+         * Source: https://www.eksworkshop.com/intermediate/230_logging/deploy/
+         */
         const doc = readYamlDocument(__dirname + '/fluent-bit.yaml');
-        const docArray = doc.replace(/{{es_endpoint}}/g, domainEndpoint)
+        const docArray = doc.replace(/{{es_endpoint}}/g, domain.domainEndpoint)
+            .replace(/{{fluent-bit-service-account}}/g, serviceAccountName)
         const manifest = docArray.split("---").map(e => loadYaml(e));
         new KubernetesManifest(cluster.stack, "fluent-bit-daemon", {
             cluster,
             manifest,
             overwrite: true
         });
+    }
+
+    protected mapIAMRoleToUser() {
+        const masterUserName = this.options?.adminUsername || this.adminUsername
+        const adminPasswordSecretKey = this.options?.adminPasswordSecretKey || this.adminPasswordSecretKey
+        const masterUserPassword = cdk.SecretValue.secretsManager(adminPasswordSecretKey)
+        const auth = Buffer.from(`${masterUserName}:${masterUserPassword}`).toString("base64")
+        const url = `https://${this.elasticSearchDomainEndpoint}/_opendistro/_security/api/rolesmapping/all_access?pretty`
+        const opts = {
+            headers: {
+                'Content Type': 'application/json',
+                'Authorization': "Basic " + auth
+            },
+            json: {
+                "op": "add",
+                "path": "/backend_roles",
+                "value": [`"'${this.fluentBitSARoleArn}'"`]
+            },
+        }
+        console.log("ES URL", url)
+        console.log("ES OPTS", opts)
+        request('PATCH', url, opts)
     }
 
     /**
